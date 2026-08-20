cce47d14a1a0f0203eebc875ffd871d11f177f43
Viet Tran Hoang <vietth2004@gmail.com>
2026-08-20 13:29:31 +0700

    Update FilePath.java

--- src/main/java/utils/FilePath.java
+++ src/main/java/utils/FilePath.java
@@ -1,8 +1,11 @@
 package utils;
 
+import static utils.common.constants.CLONED_PROJECT_PATH;
+import static utils.common.constants.UPLOADED_PROJECT_PATH;
+
 public final class FilePath {
-    public final static String clonedProjectPath = "E:\\IdeaProjects\\testDriver\\clonedProject";
-    public final static String uploadedProjectPath = "E:\\IdeaProjects\\testDriver\\uploadedProject";
+    public final static String clonedProjectPath = CLONED_PROJECT_PATH;
+    public final static String uploadedProjectPath = UPLOADED_PROJECT_PATH;
     public final static String concreteExecuteResultPath = "src/main/java/utils/autoUnitTestUtil/concreteExecuteResult.txt";
     public final static String generatedTestDataPath = "src/main/java/utils/autoUnitTestUtil/generatedTestData.txt";
     public final static String targetClassesFolderPath = "target/classes";
