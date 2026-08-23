731a6b2f76b580c13eb624a894b3a28f639c3614
Trần Hoàng Việt <33004473+vietth2004@users.noreply.github.com>
2026-08-23 23:40:56 +0700

    test LeetCode-Java-Solutions-master

--- src/main/java/utils/common/constants.java
+++ src/main/java/utils/common/constants.java
@@ -18,7 +18,7 @@ public interface constants {
 //    String TEST_FOLDER = "\\sample\\Algorithms-master.zip";
 
     //https://github.com/pedrovgs/Algorithms
-    String TEST_FOLDER = "\\sample\\JAVA-Challenges-main.zip";
+    String TEST_FOLDER = "\\sample\\LeetCode-Java-Solutions-master.zip";
 
 
 
