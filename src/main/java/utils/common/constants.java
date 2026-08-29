2d46506929211c45293a46419aa2d19259cb1de0
Trần Hoàng Việt <33004473+vietth2004@users.noreply.github.com>
2026-08-29 21:50:16 +0700

    Finished testing 1 times

--- src/main/java/utils/common/constants.java
+++ src/main/java/utils/common/constants.java
@@ -18,7 +18,7 @@ public interface constants {
 //    String TEST_FOLDER = "\\sample\\Algorithms-master.zip";
 
     //https://github.com/pedrovgs/Algorithms
-    String TEST_FOLDER = "\\sample\\intro-to-java-programming-master.zip";
+    String TEST_FOLDER = "\\sample\\LeetCode-Java-Solutions-master.zip";
 
     //https://github.com/jdereg/java-util/tree/master
 
